030e9e87fc0ae6784b39be98e451232a18718bc3
AlessandroZ <zanni.alessandro@gmail.com>
2018-03-05 15:34:10 +0100

    fix #3

--- LaZagneForensic/lazagne/config/dpapi.py
+++ LaZagneForensic/lazagne/config/dpapi.py
@@ -35,20 +35,21 @@ class Decrypt_DPAPI():
 					if folder.startswith('S-'):
 						self.sid = folder
 						break
+				
+				if self.sid:
+					masterkeydir = os.path.join(protect_folder, self.sid)
+					if os.path.exists(masterkeydir):
+						self.umkp = MasterKeyPool()
+						self.umkp.load_directory(masterkeydir)
+						self.umkp.add_credhist_file(sid=self.sid, credfile=credhist_file)
+						
+						if password:
+							for r in self.umkp.try_credential(sid=self.sid, password=password):
+								print_debug('INFO', r)
 
-				masterkeydir = os.path.join(protect_folder, self.sid)
-				if os.path.exists(masterkeydir):
-					self.umkp = MasterKeyPool()
-					self.umkp.load_directory(masterkeydir)
-					self.umkp.add_credhist_file(sid=self.sid, credfile=credhist_file)
-					
-					if password:
-						for r in self.umkp.try_credential(sid=self.sid, password=password):
-							print_debug('INFO', r)
-
-					elif pwdhash:
-						for r in self.umkp.try_credential_hash(self.sid, pwdhash=pwdhash.decode('hex')):
-							print_debug('INFO', r)
+						elif pwdhash:
+							for r in self.umkp.try_credential_hash(self.sid, pwdhash=pwdhash.decode('hex')):
+								print_debug('INFO', r)
 
 		# System Information
 
@@ -75,9 +76,10 @@ class Decrypt_DPAPI():
 									print_debug('INFO', r)
 
 	def check_credentials(self, passwords):
-		for password in passwords:
-			for r in self.umkp.try_credential(sid=self.sid, password=password):
-				print_debug('INFO', r)
+		if self.umkp:
+			for password in passwords:
+				for r in self.umkp.try_credential(sid=self.sid, password=password):
+					print_debug('INFO', r)
 
 	def manage_response(self, ok, msg):
 		if ok:
@@ -90,52 +92,59 @@ class Decrypt_DPAPI():
 		"""
 		Decrypt DPAPI Blob
 		"""
-		blob  	= DPAPIBlob(dpapi_blob)
-		ok, msg = blob.decrypt_encrypted_blob(mkp=self.umkp)
-		return self.manage_response(ok, msg)
+		if self.umkp:
+			blob  	= DPAPIBlob(dpapi_blob)
+			ok, msg = blob.decrypt_encrypted_blob(mkp=self.umkp)
+			return self.manage_response(ok, msg)
 		
 	def decrypt_cred(self, credfile):
 		""" 
 		Decrypt Credential Files
 		"""
-		c = CredFile(credfile)
-		ok, msg = c.decrypt(self.umkp)
-		return self.manage_response(ok, msg)
+		if self.umkp:
+			c = CredFile(credfile)
+			ok, msg = c.decrypt(self.umkp)
+			return self.manage_response(ok, msg)
 		
 	def decrypt_vault(self, vaults_dir):
 		""" 
 		Decrypt Vault Files
 		"""
-		v = Vault(vaults_dir=vaults_dir)
-		ok, msg = v.decrypt(mkp=self.umkp)
-		return self.manage_response(ok, msg)
+		if self.umkp:
+			v = Vault(vaults_dir=vaults_dir)
+			ok, msg = v.decrypt(mkp=self.umkp)
+			return self.manage_response(ok, msg)
 
 	def get_dpapi_hash(self, context='local'):
 		"""
 		Retrieve DPAPI hash to bruteforce it using john or hashcat.
 		"""
-		return self.umkp.get_dpapi_hash(sid=self.sid)
+		if self.umkp:
+			return self.umkp.get_dpapi_hash(sid=self.sid)
 
 	def get_cleartext_password(self):
 		"""
 		Retrieve cleartext password associated to the preferred user maskterkey. 
 		This password should represent the windows user password. 
 		"""
-		return self.umkp.get_cleartext_password()
+		if self.umkp:
+			return self.umkp.get_cleartext_password()
 
 	def decrypt_wifi_blob(self, key_material):
 		"""
 		Decrypt wifi password
 		"""
-		blob 	= DPAPIBlob(key_material.decode('hex'))
-		ok, msg = blob.decrypt_encrypted_blob(mkp=self.smkp)
-		return self.manage_response(ok, msg)
+		if self.smkp:
+			blob 	= DPAPIBlob(key_material.decode('hex'))
+			ok, msg = blob.decrypt_encrypted_blob(mkp=self.smkp)
+			return self.manage_response(ok, msg)
 
 	def decrypt_system_vault(self, vaults_dir):
 		"""
 		Decrypt System Vault
 		"""
-		v = Vault(vaults_dir=vaults_dir)
-		ok, msg = v.decrypt(mkp=self.smkp)
-		return self.manage_response(ok, msg)
+		if self.smkp:
+			v = Vault(vaults_dir=vaults_dir)
+			ok, msg = v.decrypt(mkp=self.smkp)
+			return self.manage_response(ok, msg)
 
