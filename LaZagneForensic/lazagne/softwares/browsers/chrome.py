9687234f7e471f1e39e6f3afbca61d56ee098ed2
anonymouz4 <32394757+anonymouz4@users.noreply.github.com>
2019-01-20 20:29:11 +0100

    Option to create an updated DB as Copy
    Both Features can be enabled with those two variables:
    ```
    create_updated_db = False
    export_decrypted_db_json = True
    ```

--- LaZagneForensic/lazagne/softwares/browsers/chrome.py
+++ LaZagneForensic/lazagne/softwares/browsers/chrome.py
@@ -8,6 +8,8 @@ import traceback
 import sqlite3
 import os
 import json
+from shutil import copyfile
+
 
 class Chrome(ModuleInfo):
 	def __init__(self):
@@ -68,6 +70,12 @@ class Chrome(ModuleInfo):
 		username = os.path.basename(os.path.dirname(path))
 
 		cookieFound = []
+		
+		# Will create a copy of the cookies and replace the encrypted value with the decrypted one
+		create_updated_db = False
+		
+		export_decrypted_db_json = True
+		
 	
 		cookie_database_path = os.path.join(path, profile, u'Cookies')
 	
@@ -77,53 +85,103 @@ class Chrome(ModuleInfo):
 		else:
 			print_debug('DEBUG', u'Cookie database found: {database_path}'.format(database_path=cookie_database_path))
 
-	
-		# Connect to the Database
-		try:
-			conn 	= sqlite3.connect(cookie_database_path)
-			cursor 	= conn.cursor()
-		except Exception,e:
-			print_debug('ERROR', u'An error occured opening the cookie file')
-			print_debug('DEBUG', traceback.format_exc())
-			return
+		
 		
 
-		#cursor.execute('SELECT host_key, name, value, encrypted_value FROM cookies')
-		cursor.execute('SELECT creation_utc, host_key, name, value, path, expires_utc, is_secure, is_httponly, last_access_utc, has_expires, is_persistent, priority, encrypted_value, firstpartyonly FROM cookies')
-
-		for result in cursor.fetchall():
+		if create_updated_db:
+			
 			try:
-				# Decrypt the Password
-				password = constant.user_dpapi.decrypt_blob(result[12])
-				#print('encrypted', result[1] , result[2] , result[3], password)
-				if password:
-					cookieFound.append(
-							{
-								'creation_utc'		: result[0],
-								'host_key'		: result[1],
-								'name'			: result[2],
-								'value'			: result[3],
-								'path'			: result[4],
-								'expires_utc'		: result[5],
-								'is_secure'		: result[6],
-								'is_httponly'		: result[7],
-								'last_access_utc'	: result[8],
-								'has_expires'		: result[9],
-								'is_persistent'		: result[10],
-								'priority'		: result[11],
-								'encrypted_value'	: password,
-								'firstpartyonly'	: result[13]
-							}
-					)
-			except Exception,e:
-				print_debug('DEBUG', traceback.format_exc())
+				# Make Copy of DB
+				new_db = os.getcwd() + '/' + os.path.basename(cookie_database_path) + '-%s-%s' % (username, profile)
 
-		conn.close()
-		
-		with open('cookies-%s-%s.json' % (username, profile), 'w') as fp:
-			json.dump(cookieFound, fp, indent=2)
-		
-		
+				copyfile(cookie_database_path, new_db)
 				
-		return
+			
+				# Connect to the Database
+				conn 	= sqlite3.connect(new_db)
+				cursor 	= conn.cursor()
+			except Exception,e:
+				print_debug('ERROR', u'An error occured opening the cookie file')
+				print_debug('DEBUG', traceback.format_exc())
+				return
+					
+			# Get the results
+			cursor.execute('SELECT host_key, name, value, encrypted_value FROM cookies')
+			for result in cursor.fetchall():
+				try:
+					# Decrypt the encrypted_value
+					decrypted_value = constant.user_dpapi.decrypt_blob(result[3]).decode('utf-8') or value or 0
+
+					if decrypted_value:
+						# Update the cookies with the decrypted value
+						# This also makes all session cookies persistent
+				
+						cursor.execute('\
+									   UPDATE cookies SET value = ?, has_expires = 1, expires_utc = 99999999999999999, is_persistent = 1, is_secure = 0\
+									   WHERE host_key = ?\
+									   AND name = ?',
+						(decrypted_value, result[0], result[1]));
+
+				except:
+					print_debug('DEBUG', traceback.format_exc())
+
+			conn.commit()
+			conn.close()
+				
+
+
+
+
+
+	
+		if export_decrypted_db_json:
+		
+			# Connect to the Database
+			try:
+				conn 	= sqlite3.connect(cookie_database_path)
+				cursor 	= conn.cursor()
+			except Exception,e:
+				print_debug('ERROR', u'An error occured opening the cookie file')
+				print_debug('DEBUG', traceback.format_exc())
+				return
+		
+
+			#cursor.execute('SELECT host_key, name, value, encrypted_value FROM cookies')
+			cursor.execute('SELECT creation_utc, host_key, name, value, path, expires_utc, is_secure, is_httponly, last_access_utc, has_expires, is_persistent, priority, encrypted_value, firstpartyonly FROM cookies')
+
+
+			for result in cursor.fetchall():
+				try:
+					# Decrypt the encrypted_value
+					decrypted_value = constant.user_dpapi.decrypt_blob(result[12])
+					#print_debug('encrypted', result[1] , result[2] , result[3], decrypted_value)
+					if decrypted_value:
+						cookieFound.append(
+								{
+									'creation_utc'		: result[0],
+									'host_key'			: result[1],
+									'name'				: result[2],
+									'value'				: result[3],
+									'path'				: result[4],
+									'expires_utc'		: result[5],
+									'is_secure'			: result[6],
+									'is_httponly'		: result[7],
+									'last_access_utc'	: result[8],
+									'has_expires'		: result[9],
+									'is_persistent'		: result[10],
+									'priority'			: result[11],
+									'encrypted_value'	: decrypted_value,
+									'firstpartyonly'	: result[13]
+								}
+						)
+				except Exception,e:
+					print_debug('DEBUG', traceback.format_exc())
+
+			conn.close()
+		
+			with open('cookies-%s-%s.json' % (username, profile), 'w') as fp:
+				json.dump(cookieFound, fp, indent=2)
+
+				
+			return
 	
